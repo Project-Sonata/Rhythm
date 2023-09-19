8204aea1b401aba76b2ed24e1eae475c8b5f5b9b
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-19 22:20:26 +0300

    Added 'test' profile

--- src/test/java/com/odeyalo/sonata/rhythm/RhythmApplicationTests.java
+++ src/test/java/com/odeyalo/sonata/rhythm/RhythmApplicationTests.java
@@ -2,8 +2,10 @@ package com.odeyalo.sonata.rhythm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RhythmApplicationTests {
 
     @Test
