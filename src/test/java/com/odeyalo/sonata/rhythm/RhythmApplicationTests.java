52cada282f93ffce59ec319a5efd9c8ea71ce0e7
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-19 20:05:53 +0300

    Basic project files

---
+++ src/test/java/com/odeyalo/sonata/rhythm/RhythmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeyalo.sonata.rhythm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhythmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
